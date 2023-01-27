96ac05b8564c63c98cb2f764d2c242ccab107ab8
Светлана Пантелеева <panteleevas@gmail.com>
2023-01-27 22:50:54 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            string name = strArray[0];
+            string revers = string.Empty;
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                revers += name[i];
+            }
+            if (name == revers)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+}
